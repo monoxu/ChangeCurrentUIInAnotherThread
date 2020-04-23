0b77fe60cc72a353e67558971c10e4ad35375be0
Xu <wangxu1186711291@gmail.com>
2020-04-23 09:51:47 +0800

    Create demo

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Action<int> action = ((data) =>
+                    {
+                        this.textBox1.Text = data.ToString();
+                    });
+                    Invoke(action, i);
+                }
+            });
+            
+        }
+    }
+}
